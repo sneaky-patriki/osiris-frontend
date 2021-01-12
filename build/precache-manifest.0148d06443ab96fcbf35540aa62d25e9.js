5225391b914ec65fa3d8409d94685680c52ba548
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2021-01-12 20:45:18 +0800

    build folder added

---
+++ build/precache-manifest.0148d06443ab96fcbf35540aa62d25e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ddcc8a90817bbb68c28ce4510e7b71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b6a494e67bd6e417185",
+    "url": "/static/css/main.358fd442.chunk.css"
+  },
+  {
+    "revision": "cf692f168ded276bd09d",
+    "url": "/static/js/2.479b643b.chunk.js"
+  },
+  {
+    "revision": "0602514d935c027fd8dc1b18eaa3620f",
+    "url": "/static/js/2.479b643b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b6a494e67bd6e417185",
+    "url": "/static/js/main.06e7be67.chunk.js"
+  },
+  {
+    "revision": "340bf1772271908cb534",
+    "url": "/static/js/runtime-main.2e603eba.js"
+  }
+]);
